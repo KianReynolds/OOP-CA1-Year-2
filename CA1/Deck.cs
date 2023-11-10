63f7a6824834771f1ddd8915870b810fe8e93f5c
kianr <kianr@MSI>
2023-11-10 15:57:55 +0000

    Add project files.

---
+++ CA1/Deck.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    internal class Deck
+    {
+
+        public List<Card> Cards { get; set; }
+
+
+        //get next card
+
+        public Card DrawCard()
+        {
+            Card card = Cards[0];
+            Cards.RemoveAt(0);
+            return card;
+        }
+
+
+        //create deck
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+
+            //create 52 cards
+            string[] faces = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            string[] suits = { "Hearts", "Spades", "Diamonds", "Clubs" };
+
+            for (int i = 0; i < 4; i++) //suits
+            {
+                for (int j = 0; j < 13; j++)  //faces
+                {
+                    Card c1 = new Card() { Face = faces[j], Suit = suits[i] };
+                    Cards.Add(c1);
+                }
+            }
+
+
+
+        }
+
+        public void DisplayCards()
+        {
+            foreach (Card card in Cards)
+            {
+                Console.WriteLine($"{card.Face} of {card.Suit}");
+            }
+        }
+
+        //shuufle deck
+        public void shuffleDeck()
+        {
+            Random rng = new Random();
+            int n = Cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = Cards[k];
+                Cards[k] = Cards[n];
+                Cards[n] = value;
+            }
+        }
+
+        
+
+
+    }
+
+    internal class Card
+    {
+
+        public string Face { get; set; }  //Ace, 2, 3... Jack...
+        public string Suit { get; set; }  //spades, diamonds etc
+
+        public int Value { get; set; }
+
+        string[] faces = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        string[] suits = { "Hearts", "Spades", "Diamonds", "Clubs" };
+    }
+    internal class Hand
+    {
+        private List<Card> cards; 
+        
+        public string Suit { get; set; }  //spades, diamonds etc
+
+        public string Face { get; set; }  //Ace, 2, 3... Jack...
+
+        public int Value { get; set; }
+
+
+        string[] faces = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        string[] suits = { "Hearts", "Spades", "Diamonds", "Clubs" };
+
+        public Hand()
+        {
+            cards = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+        public int GetTotal()
+        {
+            //Face = faces[rng.Next(0, 13)];
+            //Suit = suits[rng.Next(0, 4)];
+            int total = 0;  
+            int numAces=0;
+
+            foreach (Card card in cards)
+            {
+                if (Face == "Jack" || Face == "Queen" || Face == "King")
+                {
+                    Value = 10;
+                }
+                else if (Face == "Ace")
+                {
+                    Value = 11;
+                }
+                else
+                {
+                    Value = int.Parse(Face);
+                }
+            }
+
+        }
+        public override string ToString()
+        {
+            return $"{Face} of  {Suit}";
+        }
+
+    }
+    
+}
---
+++ CA1/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Security.Cryptography;
+
+namespace CA1
+{
+    internal class Program
+    {
+        static int playerScore;
+        static int dealerScore;
+
+
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            //deck.DisplayCards();
+            deck.shuffleDeck();
+
+
+
+            Random rng = new Random();
+
+            Hand c1 = new Hand(rng);
+            int player1 = c1.Value;
+
+            Hand c2 = new Hand(rng);
+            int player2 = c2.Value;
+
+            Hand c3 = new Hand(rng);
+            int player3 = c3.Value;
+
+            Hand playerHand = new Hand(rng);
+            Hand dealerHand = new Hand(rng);
+
+            playerHand.
+
+
+            //Console.ReadLine();
+
+            Console.WriteLine("Welcome, Let's Play BlackJack");
+            
+            
+            
+            Random random = new Random();
+
+
+            //get 2 numbers
+            //int player1 = random.Next(1, 10);
+            //int player2 = random.Next(1, 10);
+
+            //add together
+            playerScore = Sum(player1, player2);
+
+            //display total
+            Console.WriteLine($"The Player is dealt {player1}");
+            Console.WriteLine($"The Player is dealt {player2}");
+            Console.WriteLine($"The Players final score is {playerScore}");
+
+            //ask if you want another number
+            bool playAgain = false;
+            Console.WriteLine("Do you want to stick or twist - s/t?");
+            string response = Console.ReadLine();
+            if (response == "t")
+                playAgain = true;
+
+            while (playAgain)
+            {
+                //get another
+                player3 = random.Next(1, 10);
+                Console.WriteLine(player3);
+
+                //add to total
+                playerScore += player3;
+
+                //display total
+                Console.WriteLine($"The Players final score is {playerScore}");
+
+                //ask for playAgain
+                Console.WriteLine("Do you want to stick or twist - s/t?");
+                response = Console.ReadLine();
+                if (response == "t")
+                    playAgain = true;
+                else
+                    playAgain = false;
+            }
+
+            //if so repeat
+
+
+
+
+
+            int dealer = random.Next(1, 10);
+            
+            int dealer2 = random.Next(1,10);
+
+            
+             dealerScore = Sum(dealer, dealer2);
+            
+
+            //Console.WriteLine("Do you want to stick or twist - s/t?");
+            //string answer = Console.ReadLine();
+            //Answer(answer);
+
+
+            Console.WriteLine("Dealer plays");
+            Console.WriteLine();
+            Console.WriteLine($"The Dealer is dealt {dealer}");
+            Console.WriteLine($"The Dealer is dealt {dealer2}");
+            Console.WriteLine($"The Dealers final score is {dealerScore}");
+
+
+            if(playerScore > 21)
+            {
+                Console.WriteLine("Player bust! Dealer wins");
+            }else if(dealerScore > 21)
+            {
+                Console.WriteLine("Dealer bust! Player wins");
+            }
+            else if (playerScore > dealerScore)
+            {
+                Console.WriteLine("Player Wins");
+            }else if(playerScore < dealerScore) 
+            {
+                Console.WriteLine("Dealer Wins");
+            }else
+            {
+                Console.WriteLine("It's a tie");
+            }
+
+            Console.WriteLine("Would you like to play another round - y/n?");
+
+
+            
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            int final = 0;
+            
+            final = num1 + num2;
+            return final;
+        }
+
+        
+        
+        
+
+        
+
+
+
+
+
+
+
+    }
+}
